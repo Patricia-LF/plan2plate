a3403205030c8ddce22eee39f7fbf60e5c0b2ee8
Emilie Sengenbjerg <Emilie_Sengenbjerg@hotmail.com>
2026-01-30 16:26:39 +0100

    Start up 2nd file, works fine

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ const port = 3000;
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const filePath = path.join(__dirname, "./", "src", "/index.html");
-app.use(express.static(path.join(__dirname, "src/public/")));
+const filePathRecipe = path.join(__dirname, "./", "src", "/recipe.html");
 
 app.get("/", (req, res) => {
   res.sendFile(filePath);
 });
 
+app.get("/recipe", (req, res) => {
+  res.sendFile(filePathRecipe);
+});
+
+app.use(express.static(path.join(__dirname, "src/public/")));
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+console.log("Recipe path:", filePathRecipe);
